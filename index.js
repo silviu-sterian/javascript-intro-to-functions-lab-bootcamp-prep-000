4938cd43d86bc1efe2c0f5825ab07953fa7ed89f
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-18 17:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(str)
   if (str.toLowerCase()=='i love you, grandma')
     return 'I love you, too.';
   
- if(str.toLowerCase()==str)
-    return 'I can''t hear you';
+ if(str.toL"owerCase()==str)
+    return '"I can't hear you';
     
  if(str.toUpperCase()==str)
     return 'YES INDEED!';
